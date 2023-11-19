e5573ad15c730353203b317303a43bf9743eb783
user <asxairal@mail.ru>
2023-11-19 18:23:41 +0300

    Добавьте файлы проекта.

---
+++ Opposition/Program.cs
@@ -0,0 +1,26 @@
+﻿
+public class Program
+{
+    public static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            numbers[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (numbers[i] + numbers[j] == 0)
+                {
+                    Console.WriteLine($"Числа с индексами {i} и {j} являются противоположными.");
+                }
+
+            }
+        }
+    }
+}
